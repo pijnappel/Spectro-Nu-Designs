902dde2d49884bf7498175dabd31b6e338ef73ef
Timo Pijnappel <timo@pijnappel.de>
2019-11-15 12:24:23 +0100

    Imported Material Design Toolkit Demo Code as initial reference

---
+++ MainDemo.Wpf/Home.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Configuration;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Controls;
+using MaterialDesignDemo.Domain;
+
+namespace MaterialDesignColors.WpfExample
+{
+    /// <summary>
+    /// Interaction logic for Home.xaml
+    /// </summary>
+    public partial class Home : UserControl
+    {
+        public Home()
+        {
+            InitializeComponent();
+        }
+
+        private void GitHubButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            Link.OpenInBrowser("https://github.com/pijnappel/Spectro-Nu-Designs");
+        }
+
+        private void EmailButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            Link.OpenInBrowser("mailto://timo.pijnappel@ametek.com");
+        }
+
+      
+    }
+}
---
+++ MainDemo.Wpf/MaterialDesignToolkitDemos/MenusAndToolBars.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using MaterialDesignDemo.Domain;
+
+namespace MaterialDesignColors.WpfExample
+{
+    /// <summary>
+    /// Interaction logic for MenusAndToolBars.xaml
+    /// </summary>
+    public partial class MenusAndToolBars : UserControl
+    {
+        public MenusAndToolBars()
+        {
+            InitializeComponent();
+        }
+
+        private void TwitterButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            Link.OpenInBrowser("https://twitter.com/James_Willock");
+        }
+    }
+}
